f4441c8f23f197a2d06c7a4075a6fa39c7b5a6a8
Wim Van Renterghem <git@vrwim.com>
2016-01-12 07:44:57 +0100

    Initial Commit

---
+++ WRBaseClasses/WRBaseClasses.h
@@ -0,0 +1,19 @@
+//
+//  WRBaseClasses.h
+//  WRBaseClasses
+//
+//  Created by Wim Van Renterghem on 12/01/16.
+//  Copyright © 2016 Wim Van Renterghem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WRBaseClasses.
+FOUNDATION_EXPORT double WRBaseClassesVersionNumber;
+
+//! Project version string for WRBaseClasses.
+FOUNDATION_EXPORT const unsigned char WRBaseClassesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WRBaseClasses/PublicHeader.h>
+
+
