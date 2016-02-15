cb01b1ef853142e9968df2be438a1e379ed5094a
Wim Van Renterghem <git@vrwim.com>
2016-02-15 20:38:05 +0100

    Added demo project

---
+++ WRDemo/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  WRDemo
+//
+//  Created by Wim Van Renterghem on 13/02/16.
+//  Copyright © 2016 Wim Van Renterghem. All rights reserved.
+//
+
+import UIKit
+import WRBaseClasses
+
+class ViewController: UIViewController {
+
+    var webView: UIWebView = UIWebView()
+    var label: UILabel = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let attributedString = AttributedStringBuilder()
+        attributedString.append("Hello", style: Style(font: UIFont.systemFontOfSize(20), color: UIColor.orangeColor()))
+        attributedString.append("World", style: Style(font: UIFont.systemFontOfSize(32), color: UIColor.grayColor()))
+        attributedString.append("I", style: Style(font: UIFont.systemFontOfSize(78), color: UIColor.blackColor()))
+        attributedString.append("Am", style: Style(font: UIFont.systemFontOfSize(26), color: UIColor.purpleColor()))
+        attributedString.append("Wim", style: Style(font: UIFont.systemFontOfSize(19), color: UIColor.yellowColor()))
+        attributedString.append("Creator", style: Style(font: UIFont.systemFontOfSize(35), color: UIColor.blueColor()))
+        attributedString.append("Of", style: Style(font: UIFont.systemFontOfSize(38), color: UIColor.grayColor()))
+        attributedString.append("WRBaseClasses", style: Style(font: UIFont.systemFontOfSize(28), color: UIColor.magentaColor()))
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        view.addSubview(webView)
+        
+        label.top.Equals(view.top).active = true
+        label.leading.Equals(view.leading).active = true
+        label.trailing.Equals(view.trailing).active = true
+        
+        label.bottom.Equals(webView.top).active = true
+        label.height.Equals(webView.height).active = true
+        
+        webView.bottom.Equals(view.bottom).active = true
+        webView.leading.Equals(view.leading).active = true
+        webView.trailing.Equals(view.trailing).active = true
+        
+        webView.loadHTMLString(attributedString.HTML, baseURL: nil)
+        label.attributedText = attributedString.attributedString
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
