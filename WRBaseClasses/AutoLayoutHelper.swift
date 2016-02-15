c5721339526dfc5ce93b782bfc6ce36838af12f0
Wim Van Renterghem <git@vrwim.com>
2016-02-15 20:37:18 +0100

    Added AutoLayoutHelper

---
+++ WRBaseClasses/AutoLayoutHelper.swift
@@ -0,0 +1,51 @@
+//
+//  AutoLayoutHelper.swift
+//  WRBaseClasses
+//
+//  Created by Wim Van Renterghem on 15/02/16.
+//  Copyright © 2016 Wim Van Renterghem. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class AutoLayoutHelper {
+    public var view: UIView
+    public var attribute: NSLayoutAttribute
+    
+    public init(view: UIView, attribute: NSLayoutAttribute) {
+        self.view = view
+        self.attribute = attribute
+    }
+    
+    public func Equals( other:AutoLayoutHelper,  spacing: CGFloat = 0) -> NSLayoutConstraint
+    {
+    return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: NSLayoutRelation.Equal, toItem: other.view, attribute: other.attribute, multiplier: 1, constant: spacing)
+    }
+    
+    public func Equals( constant: CGFloat) -> NSLayoutConstraint
+    {
+    return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: constant);
+    }
+    
+    public func GreaterThanOrEquals( other: AutoLayoutHelper,  spacing: CGFloat = 0) -> NSLayoutConstraint
+    {
+    return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: NSLayoutRelation.GreaterThanOrEqual, toItem: other.view, attribute: other.attribute, multiplier: 1, constant: spacing);
+    }
+    
+    public func GreaterThanOrEquals( constant: CGFloat) -> NSLayoutConstraint
+    {
+    return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: NSLayoutRelation.GreaterThanOrEqual, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: constant);
+    }
+    
+    public func LessThanOrEquals( other: AutoLayoutHelper,  spacing: CGFloat = 0) -> NSLayoutConstraint
+    {
+    return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: NSLayoutRelation.LessThanOrEqual, toItem: other.view, attribute: other.attribute, multiplier: 1, constant: spacing);
+    }
+    
+    public func LteessThanOrEquals( constant: CGFloat) -> NSLayoutConstraint
+    {
+    return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: NSLayoutRelation.LessThanOrEqual, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: constant);
+    }
+    
+}
--- WRBaseClasses/Extensions.swift
+++ WRBaseClasses/Extensions.swift
@@ -19,3 +19,134 @@ extension UITableView {
 		}
 	}
 }
+
+// MARK: -
+
+extension UIView {
+
+	// MARK: Edges
+
+	public var left: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .Left);
+	}
+
+	public var right: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .Right);
+	}
+
+	public var top: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .Top);
+	}
+
+	public var bottom: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .Bottom);
+	}
+
+	public var leading: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .Leading);
+	}
+
+	public var trailing: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .Trailing);
+	}
+
+	// MARK: Size
+
+	public var width: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .Width);
+	}
+
+	public var height: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .Height);
+	}
+
+	// MARK: Center
+
+	public var centerX: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .CenterX);
+	}
+
+	public var centerY: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .CenterY);
+	}
+
+	public var centerXWithinMargins: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .CenterXWithinMargins);
+	}
+
+	public var centerYWithinMargins: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .CenterYWithinMargins);
+	}
+
+	// MARK: Baseline
+
+	public var baseline: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .Baseline);
+	}
+
+	public var lastBaseline: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .LastBaseline);
+	}
+
+	public var firstBaseline: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .FirstBaseline);
+	}
+
+	// MARK: Margins
+
+	public var leftMargin: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .LeftMargin);
+	}
+
+	public var rightMargin: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .RightMargin);
+	}
+
+	public var topMargin: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .TopMargin);
+	}
+
+	public var bottomMargin: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .BottomMargin);
+	}
+
+	public var leadingMargin: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .LeadingMargin);
+	}
+
+	public var trailingMargin: AutoLayoutHelper
+	{
+		return AutoLayoutHelper(view: self, attribute: .TrailingMargin);
+	}
+}
+
+// MARK: -
+
+extension NSLayoutConstraint {
+    
+    public func activate(active: Bool = true) -> NSLayoutConstraint {
+        self.active = active
+        return self
+    }
+    
+}
